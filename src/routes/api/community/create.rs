9ff93623f4c695e22c5dc6940ef33a2a52e1b668
Ivy <ivy.gifford04@gmail.com>
2025-06-05 21:24:34 -0400

    use token in header for create community

--- src/routes/api/community/create.rs
+++ src/routes/api/community/create.rs
@@ -1,43 +1,46 @@
 //! `post /api/bayou_v1/community/create`
 //!
-//! create a new community, expects a [`crate::db::types::comm::community::Communityinfo`] wrapped in a [`crate::routes::api::types::info_with_token::BearrerWithInfo`]
-//! - ok (200) community successfully created and a [`crate::db::types::comm::community::DbCommunity`]
+//! create a new community, expects a [`crate::db::types::comm::community::Communityinfo`] and
+//! a token in the header
+//! - ok (200) community successfully created and a [`crate::routes::api::types::api_community::ApiCommunity`]
 //! should be present in the body
 //! - unauthorized (401) included token is not valid, community not created
 
 use actix_web::{
-    post,
-    web::{self, Data},
-    HttpResponse, Result,
+    post, web::{self, Data}, HttpRequest, HttpResponse, Result
 };
 
 use crate::{
     db::{pg_conn::PgConn, types::comm::community::Communityinfo},
-    routes::api::types::info_with_token::BearrerWithInfo,
+    routes::api::{types::api_community::ApiCommunity, utilities::auth_header::get_auth_header},
 };
 
 #[post("/create")]
 pub async fn create(
+    req: HttpRequest,
     conn: Data<PgConn>,
-    new_community: web::Json<BearrerWithInfo<Communityinfo>>,
+    new_community: web::Json<Communityinfo>,
 ) -> Result<HttpResponse> {
-    if conn
-        .validate_auth_token(&new_community.token)
-        .await
-        .is_err()
-    {
+    let Some(token) = get_auth_header(&req) else {
+        return Ok(HttpResponse::Unauthorized()
+            .content_type("application/json; charset=utf-8")
+            .body("invalid or missing auth header"));
+    };
+    if conn.validate_auth_token(&token).await.is_err() {
         return Ok(HttpResponse::Unauthorized()
             .content_type("application/json; charset=utf-8")
             .body(""));
     }
-    let Some(user) = conn.get_user_uid(&new_community.token.uid).await else {
+
+    let Some(user) = conn.get_user_uid(&token.uid).await else {
         return Ok(HttpResponse::Unauthorized()
             .content_type("application/json; charset=utf-8")
             .body(""));
     };
-    let community = conn
-        .create_community(new_community.info.clone(), &user)
-        .await;
+    let community: ApiCommunity = conn
+        .create_community(new_community.clone(), &user)
+        .await
+        .into();
 
     Ok(HttpResponse::Ok()
         .content_type("application/json; charset=utf-8")
